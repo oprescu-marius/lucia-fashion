35b22ddceb3b89ad727700d90f4af418ebbeed03
Marius <oprescu.marius@gmail.com>
2026-05-09 19:47:13 +0300

    Adaugare pagina Lenjerii

---
+++ deploy.bat
@@ -0,0 +1,9 @@
+@echo off
+cd /d "D:\Claude Home\Projects\Lucia Fashion\lucia-fashion"
+git add .
+set /p msg="Descrie modificarea: "
+git commit -m "%msg%"
+git push
+echo.
+echo Site actualizat cu succes!
+pause
